3d403738501ee0087c32e39702a0bd9a9f2f5132
TheWanou21 <147034259+TheWanou21@users.noreply.github.com>
2024-04-05 10:43:48 +0200

    Update fct draw.text

--- fct draw.py
+++ fct draw.py
@@ -1,17 +1,16 @@
 import pygame
+from game import *
 
-text_size = 20
-text_font = pygame.font.SysFont("Arial",text_size)
+class Texte :
+  def __init__(self,game:Game) -> None:
+    self.screen = game.screen
+    self.text_size = 20
+    self.text_font = pygame.font.SysFont("Arial",self.text_size)
 
-def draw_text(text,font,text_color,x,y,vert_mrg,left_mrg):
-    txt = font.render(text, True, text_color)
-    text_surface = pygame.Surface( (2*left_mrg + txt.get_size()[0], 2*vert_mrg + txt.get_size()[1]), pygame.SRCALPHA)
-    # Définir la couleur du rectangle avec une opacité de 20 (sur 255)
-    pygame.draw.rect(text_surface, (0, 0, 0, 120), text_surface.get_rect(), 0)
-    # Rendre le texte sur la surface transparente
+  def draw_text(self,text,font,text_color,x,y,vert_mrg,left_mrg):
+      txt = font.render(text, True, text_color)
+      text_surface = pygame.Surface( (2*left_mrg + txt.get_size()[0], 2*vert_mrg + txt.get_size()[1]), pygame.SRCALPHA)
+      pygame.draw.rect(text_surface, (0, 0, 0, 120), text_surface.get_rect(), 0)
+      self.screen.blit(text_surface, (x - left_mrg, y - vert_mrg))
+      self.screen.blit(txt, (x, y))
 
-    # Dessiner la surface avec le texte sur l'écran
-    screen.blit(text_surface, (x - left_mrg, y - vert_mrg))
-    screen.blit(txt, (x, y))
-
-  draw_text("Hello World ! Le problème c. ", text_font, (255,255,255),200,500,15,30)
