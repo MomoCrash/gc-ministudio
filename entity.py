1d015fdc8aae681b356bb523613f3167f81ade84
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2024-04-08 17:19:23 +0200

    fix

--- entity.py
+++ entity.py
@@ -3,182 +3,222 @@ from linkedlist import LinkedList
 from vector import Vector2
 from gameobject import GameObject
 from thetimer import Timer
-from texture import Sprite, SpriteSheet, Assets, SpritesRef, SpriteSheetsRef
+from texture import Sprite, SpriteSheet, Assets, SpriteSheetsRef, SpritesRef
+from math import cos, sin, pi
 
 
+class Entity(GameObject):
 
-class Entity( GameObject ):
-    
     def __init__(
-                    self,
-                    
-                    position: Vector2 = Vector2( 0, 0 ),
-                    rotation: Vector2 = Vector2( 0, 0 ),
-                    scale: Vector2 = Vector2( 1, 1 ),
-                    
-                    spriteRef: SpritesRef = None,
-                    spriteSheetRef: SpriteSheetsRef = None,
-                    spriteDimensions: Vector2 = Vector2( 1, 1 ),
-                    isVisible: bool = True,
-                    
-                    velocity: Vector2 = Vector2( 0, 0 ) #? Vector2( 4, 4 )
-                ):
-        super().__init__( position, rotation, scale, spriteRef, spriteSheetRef, spriteDimensions, isVisible )
+            self,
+
+            position: Vector2 = Vector2(0, 0),
+            rotation: Vector2 = Vector2(0, 0),
+            scale: Vector2 = Vector2(1, 1),
+
+            sprite: Sprite = None,
+            spriteSheet: SpriteSheet = None,
+            spriteDimensions: Vector2 = Vector2(0, 0),
+            isVisible: bool = True,
+
+            velocity: Vector2 = Vector2(0, 0)  # ? Vector2( 4, 4 )
+    ):
+        super().__init__(position, rotation, scale, sprite, spriteSheet, spriteDimensions, isVisible)
         self.velocity: Vector2 = velocity
-    
-    def update( self, surface: pygame.Surface, deltaTime: int ) -> None:
-        self.transform.position.addToSelf( self.velocity.multiplyToNew( deltaTime ) )
-        self.draw( surface )
+
+    def update(self, surface: pygame.Surface, deltaTime: int) -> None:
+        self.transform.position.addToSelf(self.velocity.multiplyToNew(deltaTime))
+        self.draw(surface)
 
 
-
-class Player( Entity ):
+class Player(Entity):
     def __init__(
-                    self,
-                    
-                    position: Vector2 = Vector2( 0, 0 ),
-                    rotation: Vector2 = Vector2( 0, 0 ),
-                    scale: Vector2 = Vector2( 1, 1 ),
-                    
-                    walkingLeftSpriteSheetRef: SpriteSheetsRef = None,
-                    walkingRightSpriteSheetRef: SpriteSheetsRef = None,
-                    spriteDimensions: Vector2 = Vector2( 1, 1 ),
-                    isVisible: bool = True,
-                    
-                    velocity: Vector2 = Vector2( 0, 0 ), #? Vector2( 4, 4 )
-                    maxSpeed: float = 4,
-                    jumpHeight: float = 1,
-                    gravity: float = 5
-                ):
-        super().__init__( position, rotation, scale, None, walkingRightSpriteSheetRef, spriteDimensions, isVisible, velocity )
-        self.walkingLeftSpriteSheet: SpriteSheetsRef = walkingLeftSpriteSheetRef
-        self.walkingRightSpriteSheet: SpriteSheetsRef = walkingRightSpriteSheetRef
+            self,
+
+            position: Vector2 = Vector2(0, 0),
+            rotation: Vector2 = Vector2(0, 0),
+            scale: Vector2 = Vector2(1, 1),
+
+            sprite: Sprite = None,
+            spriteSheet: SpriteSheet = None,
+            spriteDimensions: Vector2 = Vector2(0, 0),
+            isVisible: bool = True,
+
+            velocity: Vector2 = Vector2(0, 0),  # ? Vector2( 4, 4 )
+            maxSpeed: float = 4,
+            jumpHeight: float = 1,
+            gravity: float = 5
+    ):
+        super().__init__(position, rotation, scale, sprite, spriteSheet, spriteDimensions, isVisible, velocity)
         self.maxSpeed: float = maxSpeed
         self.jumpHeight: float = jumpHeight
         self.gravity: float = gravity
         self.isJumping: bool = False
         self.jumpCount: int = 10
         self.isFacingRight = False
-    
-    def update( self, surface: pygame.Surface, mapElements: list[ GameObject ] ) -> None:
+        self.CanShoot = True
+        self.shoot_timer = Timer(1, self.enableThrow)
+        self.ArrowSpeed = 500
+        self.arrow: GameObject = None
+        self.Vecteur_directeur = Vector2(0, 0)
+        self.ArrowDistance = 400
+        self.circle = pi / self.ArrowDistance
+
+    def update(self, surface: pygame.Surface, mapElements: list[GameObject]) -> None:
         pressedKey = pygame.key.get_pressed()
-        self.playerMovement( pressedKey, mapElements )
-        self.playerJump( pressedKey, mapElements )
-        
-        self.transform.position.addToSelf( self.velocity )
-        
-        if ( self.velocity.x < 0 and self.spriteSheetRef != self.walkingLeftSpriteSheet ): self.spriteSheetRef = self.walkingLeftSpriteSheet
-        elif ( self.velocity.x > 0 and self.spriteSheetRef != self.walkingRightSpriteSheet ): self.spriteSheetRef = self.walkingRightSpriteSheet
-        self.draw( surface )
-    
-    def playerMovement( self, pressedKey: pygame.key.ScancodeWrapper, mapElements: list[ GameObject ] ) -> None:
-        leftPressed: bool = pressedKey[ pygame.K_q ]
-        rightPressed: bool = pressedKey[ pygame.K_d ]
-        
-        if ( leftPressed and not rightPressed ):
-            
-            if ( self.velocity.x == 0 ):
+        self.playerMovement(pressedKey, mapElements)
+        self.playerJump(pressedKey, mapElements)
+
+        self.transform.position.addToSelf(self.velocity)
+        self.draw(surface)
+
+    def UpdateArrow(self, dt):
+        if self.arrow != None:
+            total_distance_x = abs(self.arrow.transform.position.x - self.startpos)
+
+            if total_distance_x >= self.ArrowDistance:
+                self.arrow = None
+                self.enableThrow()
+                return
+
+            self.arrow.transform.position.x += self.Vecteur_directeur.x * self.ArrowSpeed * dt
+            self.arrow.transform.position.y -= (cos((total_distance_x / self.ArrowDistance) * pi) * 7)
+
+    def playerMovement(self, pressedKey: pygame.key.ScancodeWrapper, mapElements: list[GameObject]) -> None:
+        leftPressed: bool = pressedKey[pygame.K_q]
+        rightPressed: bool = pressedKey[pygame.K_d]
+
+        if (leftPressed and not rightPressed):
+
+            if (self.velocity.x == 0):
                 self.velocity.x = -1
-            
-            if ( self.velocity.x > 0 ):
+
+            if (self.velocity.x > 0):
                 factor: float = 0.6
             else:
                 factor: float = 1.5
 
-        elif ( rightPressed and not leftPressed ):
-            
-            if ( self.velocity.x == 0 ):
+        elif (rightPressed and not leftPressed):
+
+            if (self.velocity.x == 0):
                 self.velocity.x = 1
-            
-            if ( self.velocity.x < 0 ):
+
+            if (self.velocity.x < 0):
                 factor: float = 0.6
             else:
                 factor: float = 1.5
-        
+
         else:
             factor: float = 0.8
-        
-        if ( abs( self.velocity.x * factor ) <= self.maxSpeed and self.velocity.x != 0 ):
-            self.velocity.x *= factor if abs( self.velocity.x ) > 0.9 else 0
-        
-        collision: bool = False
+
+        if (abs(self.velocity.x * factor) <= self.maxSpeed and self.velocity.x != 0):
+            self.velocity.x *= factor if abs(self.velocity.x) > 0.9 else 0
+
         self.transform.position.x += self.velocity.x
-        for mapObject in mapElements: collision = collision or self.getCollision( mapObject )
+        collision: bool = False
+        for mapObject in mapElements: collision = collision or self.getCollision(mapObject)
         self.transform.position.x -= self.velocity.x
-        if ( collision ) : self.velocity.x = 0
-    
-    def playerJump( self, pressedKey: pygame.key.ScancodeWrapper, mapElements: list[ GameObject ] ):
-        spacePressed = pressedKey[ pygame.K_SPACE ]
-        
-        self.velocity.y = ( spacePressed * self.jumpHeight ) + self.gravity
-        
-        if ( spacePressed and not self.isJumping ):
+        if (collision): self.velocity.x = 0
+
+    def playerJump(self, pressedKey: pygame.key.ScancodeWrapper, mapElements: list[GameObject]):
+        spacePressed = pressedKey[pygame.K_SPACE]
+
+        self.velocity.y = (spacePressed * self.jumpHeight) + self.gravity
+
+        if (spacePressed and not self.isJumping):
             self.isJumping = True
-        
-        if ( self.isJumping ):
-            if ( self.jumpCount >= 0 ):
-                self.velocity.y -= ( self.jumpCount * abs( self.jumpCount ) ) * 0.1
+
+        if (self.isJumping):
+            if (self.jumpCount >= 0):
+                self.velocity.y -= (self.jumpCount * abs(self.jumpCount)) * 0.1
                 self.jumpCount -= 1
             else:
                 # This will execute if our jump is finished
                 self.jumpCount = 20
                 self.isJumping = False
-        
-        collision: bool = False
+
         self.transform.position.y += self.velocity.y
-        for mapObject in mapElements: collision = collision or self.getCollision( mapObject )
+        collision: bool = False
+        for mapObject in mapElements: collision = collision or self.getCollision(mapObject)
         self.transform.position.y -= self.velocity.y
-        if ( collision ) : self.velocity.y = 0
+        if (collision): self.velocity.y = 0
+
+    def is_flip(self):
+        pressedKey = pygame.key.get_pressed()
+        if pressedKey[pygame.K_d] - pressedKey[pygame.K_q] < 0 and self.isFacingRight:
+            self.isFacingRight = False
+        elif pressedKey[pygame.K_d] - pressedKey[pygame.K_q] > 0 and not self.isFacingRight:
+            self.isFacingRight = True
 
     def Attack(self):
         print("left click")
+        if self.CanShoot:
+            self.MousePos = pygame.mouse.get_pos()
+            self.arrow = GameObject(position=Vector2(self.transform.position.x, self.transform.position.y))
+            # self.Vecteur_directeur = Vector2(self.MousePos[0] - self.transform.position.x, self.MousePos[1] - self.transform.position.y)
+            if self.isFacingRight:
+                self.Vecteur_directeur = Vector2(1, 0.5)
+            else:
+                self.Vecteur_directeur = Vector2(-1, 0.5)
+
+            self.Vecteur_directeur.normalizeToSelf()
+            self.CanShoot = False
+            self.shoot_timer.start()
+            self.startpos = self.arrow.transform.position.x
+
+    def enableThrow(self):
+        self.CanShoot = True
+
+    def DrawArrow(self, window):
+        if self.arrow != None:
+            Assets.GetSprite(SpritesRef.TOMAHAWK).draw(window, self.arrow.transform.position,
+                                                       self.arrow.transform.scale)
 
 
-class Mob( Entity ):
-    
+class Mob(Entity):
+
     def __init__(
-                    self,
-                    
-                    position: Vector2 = Vector2( 0, 0 ), #? Vector2( 500, 500 )
-                    rotation: Vector2 = Vector2( 0, 0 ),
-                    scale: Vector2 = Vector2( 1, 1 ),
-                    
-                    sprite: Sprite = None,
-                    spriteSheet: SpriteSheet = None,
-                    spriteDimensions: Vector2 = Vector2( 1, 1 ),
-                    isVisible: bool = True,
-                    
-                    velocity: Vector2 = Vector2( 0, 0 ), #? Vector2( 3, 3 )
-                    
-                    isOnLoop: bool = False,
-                    pathPositions: LinkedList = None
-                ):
-        super().__init__( position, rotation, scale, sprite, spriteSheet, spriteDimensions, isVisible, velocity )
+            self,
+
+            position: Vector2 = Vector2(0, 0),  # ? Vector2( 500, 500 )
+            rotation: Vector2 = Vector2(0, 0),
+            scale: Vector2 = Vector2(1, 1),
+
+            sprite: Sprite = None,
+            spriteSheet: SpriteSheet = None,
+            spriteDimensions: Vector2 = Vector2(0, 0),
+            isVisible: bool = True,
+
+            velocity: Vector2 = Vector2(0, 0),  # ? Vector2( 3, 3 )
+
+            isOnLoop: bool = False,
+            pathPositions: LinkedList = None
+    ):
+        super().__init__(position, rotation, scale, sprite, spriteSheet, spriteDimensions, isVisible, velocity)
         self.isOnLoop: bool = isOnLoop
-        if ( self.isOnLoop ): self.pathDestination: LinkedList = pathPositions.first
-        
+        if (self.isOnLoop): self.pathDestination: LinkedList = pathPositions.first
+
         self.maximum_distance = 400
         self.maximum_throw_distance = 700
         self.CanThrow = True
         self.ThrowSpeed = 200
-        self.Vecteur_directeur = pygame.Vector2(0,0)
+        self.WalkSpeed = 100
+        self.Vecteur_directeur = Vector2(0, 0)
         self.hammer = None
-        self.shoot_timer = Timer(3, self.enableThrow )
+        self.shoot_timer = Timer(3, self.enableThrow)
         self.isFacingRight = False
 
-    def movement( self, player: Player ):
-        
-        if self.transform.position.distanceTo( player.transform.position ) < self.maximum_distance:
-            if self.transform.position.x > self.transform.position.x:
-                self.transform.position.x += 1
+    def movement(self, player: Player, dt):
+        # running to player
+        if self.transform.position.distanceTo(player.transform.position) < self.maximum_distance:
+            if player.transform.position.x > self.transform.position.x:
+                self.transform.position.x += self.WalkSpeed * dt
                 self.isFacingRight = True
-            if self.transform.position.x < self.transform.position.x:
-                self.transform.position.x -= 1
+            if player.transform.position.x < self.transform.position.x:
+                self.transform.position.x -= self.WalkSpeed * dt
                 self.isFacingRight = False
 
-           
     def update(self, dt):
-
         self.shoot_timer.update(dt)
 
         if self.hammer != None:
@@ -186,16 +226,17 @@ class Mob( Entity ):
             self.hammer.transform.position.y += self.Vecteur_directeur.y * self.ThrowSpeed * dt
 
     def enableThrow(self):
-        print("OK")
         self.CanThrow = True
 
-    def tryThrow( self, player: Player ):
+    def tryThrow(self, player: Player):
         if self.CanThrow == False:
             return
 
-        if self.maximum_distance < self.transform.position.distanceTo( player.transform.position ) < self.maximum_throw_distance:
-            self.hammer = GameObject(position=self.transform.position, scale=Vector2(40, 80))
-            self.Vecteur_directeur = Vector2(player.transform.position.x - self.transform.position.x, player.transform.position.y - self.transform.position.y)
+        if self.maximum_distance < self.transform.position.distanceTo(
+                player.transform.position) < self.maximum_throw_distance:
+            self.hammer = GameObject(position=Vector2(self.transform.position.x, self.transform.position.y))
+            self.Vecteur_directeur = Vector2(player.transform.position.x - self.transform.position.x,
+                                             player.transform.position.y - self.transform.position.y)
             self.Vecteur_directeur.normalizeToSelf()
             self.CanThrow = False
             self.shoot_timer.start()
@@ -208,10 +249,11 @@ class Mob( Entity ):
         rect = new_image.get_rect(center=rect.center)
 
     def draw(self, window: pygame.Surface, player: Player):
-        #pygame.draw.line(window, (255,255,255), (self.rect_transform.x + self.width // 2 ,self.rect_transform.y + self.height // 2), (self.Vecteur_directeur.x * self.maximum_throw_distance + player.rect_transform.width // 2  , self.Vecteur_directeur.y * self.maximum_throw_distance  + player.rect_transform.height // 2), 1)
+        # pygame.draw.line(window, (255,255,255), (self.transform.position.x + self.transform.scale.x // 2 ,self.transform.position.y + self.transform.scale.y // 2), (self.Vecteur_directeur.x * self.maximum_throw_distance + player.transform.scale.x // 2  , self.Vecteur_directeur.y * self.maximum_throw_distance  + player.transform.scale.y // 2), 1)
         if self.hammer != None:
-            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_LEFT).draw(pygame.time.get_ticks(),window, self.hammer.transform.position, self.hammer.transform.scale)
+            Assets.GetSprite(SpritesRef.TOMAHAWK).draw(window, self.hammer.transform.position,
+                                                       self.hammer.transform.scale)
 
-        
 
-        
+
+
--- game.py
+++ game.py
@@ -1,11 +1,7 @@
 import pygame
-from linkedlist import LinkedList
-from vector import Vector2
 from map import Map
-from text import Text
-from entity import Entity, Player, Mob
-from texture import SpritesRef, SpriteSheetsRef, Sprite, SpriteSheet, Assets
-
+from texture import *
+from player import *
 
 
 class Game:
@@ -16,130 +12,107 @@ class Game:
         self.fps = fps
         self.dt = 0
         self.current_dt = 0
-        self.elementsOnScreen: LinkedList = LinkedList()
-        self.elementsOffScreen: LinkedList = LinkedList()
 
-        self.map = Map( "map1.txt", win_width, win_height )
-        self.map.load_map()
-        
+        self.map = Map(self, win_width, win_height)
+        self.map.create_object(0, 850, 3000, 20)
+        self.map.create_object(1000, 900, 200, 80)
+        self.map.create_object(1500, 650, 250, 80)
+        self.map.create_object(2500, 650, 250, 80)
+
         pygame.init()
-
         self.screen = pygame.display.set_mode((win_width, win_height))
         self.surface = pygame.display.get_surface()
         self.clock = pygame.time.Clock()
 
-        self.player = Player(
-                                position = Vector2( 1000, 500 ),
-                                walkingLeftSpriteSheetRef = SpriteSheetsRef.PLAYER_WALK_LEFT,
-                                walkingRightSpriteSheetRef = SpriteSheetsRef.PLAYER_WALK_RIGHT,
-                                spriteDimensions = Vector2( 40, 80 )
-                            )
-        self.mob = Mob( position=Vector2( 500, 500 ), scale=Vector2( 40, 80 ) )
+        self.player = Player(1000, 500, 40, 80)
+        self.mob = Mob(500, 500, 40, 80)
         self.camera = pygame.Vector2(0, 0)
 
-        self.text = Text(self.screen, "Arial")
-        
-        self.background_sprites = [Assets.GetSprite(SpritesRef.BACKGROUND_0),Assets.GetSprite(SpritesRef.BACKGROUND_0)]
+        self.background_sprites = [Assets.GetSprite(SpritesRef.BACKGROUND_0), Assets.GetSprite(SpritesRef.BACKGROUND_0)]
 
         self.loop()
 
     def inputs(self) -> bool:
 
-        self.camera.x = self.player.transform.position.x - self.width // 4
-        self.camera.y = self.player.transform.position.y - self.height // 4
+        self.player.movement()
+        self.player.is_flip()
+
+        self.camera.x = self.player.rect_transform.x - self.width // 4
+        self.camera.y = self.player.rect_transform.y - self.height // 4
 
         self.camera.x = max(0, min(self.camera.x, self.width))
         self.camera.y = max(0, min(self.camera.y, self.height))
 
-        for mapObject in self.map.decors:
-            mapObject.transform.position.x = mapObject.initial_position.x - self.camera.x
-
-        for mapObject in self.map.colliders:
-            mapObject.transform.position.x = mapObject.initial_position.x - self.camera.x
+        for mapObject in self.map.elements:
+            mapObject.rect_transform.x = mapObject.position.x - self.camera.x
 
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 return False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1: 
+                if event.button == 1:
                     self.player.Attack()
         return True
-    
-    
-    def update(self):
-        pass
 
     def update_graphics(self):
 
+        self.map.draw(self.surface)
+
         for i, segment in enumerate(self.background_sprites):
             self.surface.blit(segment.texture, (i * self.width - self.camera.x, 0))
-        
-        self.player.update( self.surface, self.map.elements )
 
-        self.map.draw(self.screen)
-
-        #REMOVE LATER
-        for collider in self.map.colliders:
-            collider.draw(self.screen, (0, 255, 0))
-       
         # Draw the player flipped on the good side
-        if self.player.isFacingRight:
-            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_RIGHT).draw(pygame.time.get_ticks(), self.surface, self.player.transform.position, self.player.transform.scale)
+        if self.player.IsFacingRight:
+            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_RIGHT).draw(pygame.time.get_ticks(), self.surface,
+                                                                          self.player.rect_transform)
         else:
-            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_LEFT).draw(pygame.time.get_ticks(), self.surface, self.player.transform.position, self.player.transform.scale)
+            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_LEFT).draw(pygame.time.get_ticks(), self.surface,
+                                                                         self.player.rect_transform)
 
         if self.mob.isFacingRight:
-            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_RIGHT).draw(pygame.time.get_ticks(), self.surface, self.mob.transform.position, self.mob.transform.scale)
+            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_RIGHT).draw(pygame.time.get_ticks(), self.surface,
+                                                                          self.mob.transform.position,
+                                                                          self.mob.transform.scale)
+
         else:
-            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_LEFT).draw(pygame.time.get_ticks(), self.surface, self.mob.transform.position, self.mob.transform.scale)
+            Assets.GetSpriteSheet(SpriteSheetsRef.PLAYER_WALK_LEFT).draw(pygame.time.get_ticks(), self.surface,
+                                                                         self.mob.rect_transform)
 
         # Develop in progress
-        self.mob.movement(self.player)
+        self.mob.movement(self.player, self.dt)
 
         self.mob.tryThrow(self.player)
         self.mob.update(self.dt)
         self.mob.draw(self.surface, self.player)
 
-        self.text.draw_text("Test de Text adaptatif !", (255, 255, 255), 100, 100, 10, 10)
+        self.player.DrawArrow(self.surface)
 
-        #Assets.GetSprite(SpritesRef.LIGHT).draw(self.surface, (self.camera.x ,self.camera.y))
+        self.player.UpdateArrow(self.dt)
 
-        
+        # for mapObject in self.map.elements:
+        #    mapObject.draw(self.surface)
+
+        # Assets.GetSprite(SpritesRef.LIGHT).draw(self.surface, (self.camera.x ,self.camera.y))
 
         pygame.display.flip()
 
     def loop(self):
-        
         running = True
-        
         while running:
             dt_start = pygame.time.get_ticks()
+
             running = self.inputs()
-            
-            # element = self.elementsOnScreen.first
-            # amountOfElementsOnScreen = self.elementsOnScreen.count
-            # for elementIndex in range( amountOfElementsOnScreen ):
-            #     if ( not element.value.isOnScreen ):
-                    
-            #         if ( amountOfElementsOnScreen == 1 ): self.elementsOnScreen.first = None
-            #         else:
-            #             element.previous.next = element.next
-            #             element.next.previous = element.previous
-            #             if ( elementIndex == 0 ):
-            #                 self.elementsOnScreen.first = element.next
-                    
-            #         self.elementsOnScreen.count -= 1
-            #         element.value.isVisible = False
-            #     element.value.update( self.surface )
-            #     element = element.next #! I think this won't work because it's not a "pointer" copy, but a real copy that creates a new Node
+
+            for mapObject in self.map.elements:
+                self.player.check_collision(mapObject.rect_transform)
 
             self.update_graphics()
 
             self.clock.tick(60)
-            
+
             dt_end = pygame.time.get_ticks()
-            self.dt =self.clock.get_time() / 1000
+            self.dt = self.clock.get_time() / 1000
             self.current_dt += self.dt
-        
         pygame.quit()
+
