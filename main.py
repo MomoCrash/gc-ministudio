2413d738461ff3bd79a12934895e2e2959df0563
Nova564 <NassimZakri@gmail.com>
2024-04-11 01:06:14 +0200

    enfin sa grand mere

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from texture import Assets
 import settings
-from editor import Editor
 from startmenu import StartMenu
 from game import Game
+from pauseUI import Menu  
 import pygame
-
 
 def init_game():
     pygame.display.init()
@@ -17,14 +16,22 @@ def init_game():
 
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     Assets.Init()
 
+=======
+>>>>>>> Stashed changes
     init_game()
 
     screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT), pygame.FULLSCREEN)
 
     menu = StartMenu(screen)
+<<<<<<< Updated upstream
 
     game = Game(screen, menu.chapter, settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT, "Utltimate game of the Year")
     
     #editor = Editor(settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT, "EDITEUR DE JEU")
+=======
+    pause_menu = Menu(screen) 
+    game = Game(screen, menu.chapter, settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT, "Ultimate Game of the Year", pause_menu)  
+>>>>>>> Stashed changes
