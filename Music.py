0f017db10a5f4eaa2a49c5c577427f344e4f14af
TheWanou21 <147034259+TheWanou21@users.noreply.github.com>
2024-04-10 12:12:39 +0200

    Update Music.py

--- Music.py
+++ Music.py
@@ -8,3 +8,15 @@ pygame.init()
 class Songs:
     def __init__(self) -> None:
         pass
+
+    def Play_Menu():
+        pygame.mixer.music.load('Musics/Projet_Vikings_Menu.mp3')
+        pygame.mixer.music.play(-1,0.0,3000)
+
+    def Play_Tuto():
+        pygame.mixer.music.load('Musics/Musique_Tuto.mp3')
+        pygame.mixer.music.play(-1,0.0,3000)
+
+    def Play_Level():
+        pygame.mixer.music.load('Musics/Musique_Level.mp3')
+        pygame.mixer.music.play(-1,0.0,3000)
